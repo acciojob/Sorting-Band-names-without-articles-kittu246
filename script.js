b224b50f752a24ae277e459b5f7a191e622a285e
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-11 13:03:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'a Victoria Memorial', 'Tajmahal'];
+
+let regex =/\bthe\b|\ban\b|\ba\b/gi ;
+let list_items =[];
+let mp ={};
+let final=[];
+
+for(let i=0;i<touristSpots.length;i++){
+
+    let items=touristSpots.replace(regex,"").trim();
+    list_items.push(items);
+    mp[items] = touristSpots[i];
+}
+
+list_items.sort();
+
+for(let i of list_items) {
+
+    final.push(mp[i]);
+}
+
+console.log(final);
+
 
